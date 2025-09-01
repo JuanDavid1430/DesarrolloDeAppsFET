4634ce895de9535d9916e44c562d45b9cb29176b
JuanDavid1430 <92183205+JuanDavid1430@users.noreply.github.com>
2025-08-31 20:53:59 -0500

    Se desarrollan actividades de ejerciio 1 en angular y se mejora diseño general

--- Clase2/ejercicioTypescript/src/app/app.component.ts
+++ Clase2/ejercicioTypescript/src/app/app.component.ts
@@ -1,18 +1,59 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ejercicioTypescript';
+  
+  elementos: string[] = [];
+  nuevoElemento: string = '';
+  elementoAEliminar: string = '';
+  tituloOriginal = 'Manipulación del DOM';
+  tituloActual = 'Manipulación del DOM';
+  fondoOriginal = '';
+  fondoActual = '';
+  mostrarRestaurar = false;
 
+  cambiarTexto() {
+    this.tituloActual = 'He manipulado el texto';
+    this.mostrarRestaurar = true;
+  }
 
-  agregarObjeto(objetivo: string) {
-    console.log("Objetivo agregado:", objetivo);
+  cambiarFondo() {
+    this.fondoActual = 'lightblue';
+    this.mostrarRestaurar = true;
+  }
+
+  restaurar() {
+    this.tituloActual = this.tituloOriginal;
+    this.fondoActual = this.fondoOriginal;
+    this.mostrarRestaurar = false;
+  }
+
+  agregarElemento() {
+    if (this.nuevoElemento.trim() !== '') {
+      this.elementos.push(this.nuevoElemento);
+      this.nuevoElemento = '';
+    } else {
+      alert('Por favor, ingresa un texto válido.');
+    }
+  }
+
+  eliminarElemento() {
+    const indice = parseInt(this.elementoAEliminar);
+    if (!isNaN(indice) && indice >= 0 && indice < this.elementos.length) {
+      this.elementos.splice(indice, 1);
+      this.elementoAEliminar = '';
+    } else {
+      alert('Por favor, ingresa un número válido.');
+    }
   }
 }
